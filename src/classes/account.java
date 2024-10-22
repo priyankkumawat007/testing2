fa16126d3c5aa0b1e13902b76f4fc7334be9c4fc
priyankkumawat007 <priyank.kumawat@fexle.com>
2024-10-22 18:17:53 +0530

    code update updateAccount

--- src/classes/account.java
+++ src/classes/account.java
@@ -1,16 +1,9 @@
 public class AccountHandler {
-
-    public class AccountHandler {
-        public void handleAccount() {
-            System.debug('Handling account');
-        }
-     
-        public void addContact(Contact con) {
-            System.debug('Adding contact: ' + con.LastName);
-        }
+    public void handleAccount() {
+        System.debug('Handling account');
     }
-    
-	public void handleAccount() {
-    	System.debug('Handling account');
-	}
+ 
+    public void addContact(Contact con) {
+        System.debug('Adding contact: ' + con.LastName);
+    }
 }
